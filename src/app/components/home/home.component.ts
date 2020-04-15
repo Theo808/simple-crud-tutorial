5f552280f96bccf6b7fd0ed1ba6df61a849427bc
Theo808 <theo.schenkyr@gmx.de>
2020-04-15 13:28:33 +0200

    New

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public title: string = "Hallo Benutzer!";
+
+
   constructor() { }
 
   ngOnInit(): void {
